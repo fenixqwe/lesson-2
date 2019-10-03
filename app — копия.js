93b1dfec45fab427794d8ab16194ac26a21b00f9
fenixqwe <53240682+fenixqwe@users.noreply.github.com>
2019-10-03 21:37:51 +0300

    Add files via upload

---
+++ app — копия.js
@@ -0,0 +1,34 @@
+//1. Создать объект с полем product, равным ‘iphone’
+//2. Добавить в объект поле price, равное 1000 и поле currency, равное ‘dollar’
+/*const object = {
+	product: 'iphone'
+};
+
+object.price = 1000;
+object.currency = 'dollar';
+
+value = object.'details': {
+	model: 
+	color: 
+};
+
+
+
+console.log(object.currency)*/
+
+
+
+
+// 1.Записать в виде switch case следующее условие
+/*let a = 0;
+
+switch (a){
+	case 'block':
+	console.log('block');break;
+	case 'none':
+	console.log('none');break;
+	case 'inline':
+	console.log('inline');break;
+	default :
+	console.log('other')
+};*/
